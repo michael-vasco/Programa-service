cae4dd81134c4129162a53a036528ffbd5bc5bd2
MAICOL_ESTIVEN_VASCO_CARDENAS <michael.vasco01@uceva.edu.co>
2025-11-23 18:04:28 -0500

    estructura inicial

---
+++ src/test/java/co/edu/uceva/programaservice/ProgramaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.programaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
